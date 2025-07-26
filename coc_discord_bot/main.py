bcbfd2aaf097d7e0b46138c144e849a7aacee0c5
Rizwan Riaz <iamrizwan077@gmail.com>
2025-07-26 22:17:03 +0500

    Initial commit

---
+++ coc_discord_bot/main.py
@@ -0,0 +1,55 @@
+import discord
+import os
+import random
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+intents = discord.Intents(messages=True, guilds=True)
+
+# client = discord.Client(intents=intents)
+
+
+token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
+
+    print(f'Message {user_message} by {username} on {channel}')
+
+    if message.author == client.user:
+        return
+
+    if channel == "bot_commands":
+        if user_message.lower() == "hello" or user_message.lower() == "hi":
+            await message.channel.send(f'Hello {username}')
+            return
+        elif user_message.lower() == "bye":
+            await message.channel.send(f'Bye {username}')
+        elif user_message.lower() == "tell me a joke":
+            jokes = [" Can someone please shed more\
+            light on how my lamp got stolen?",
+                     "Why is she called llene? She\
+                     stands on equal legs.",
+                     "What do you call a gazelle in a \
+                     lions territory? Denzel."]
+            await message.channel.send(random.choice(jokes))
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+# Assume client refers to a discord.Client subclass...
+client.run(token, log_handler=handler, log_level=logging.DEBUG)
